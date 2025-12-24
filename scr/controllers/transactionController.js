a57c1e6fa80285cdefadb31c3989e5e19148b6af
Agent610 <BhavsarParthD96@Gmail.com>
2025-12-24 11:46:20 -0500

    Update and delete functionatlity for transaction

--- scr/controllers/transactionController.js
+++ scr/controllers/transactionController.js
@@ -2,7 +2,7 @@ const Transaction = require("../models/transaction");
 
 const getTransactions = async (req, res) => {
   try {
-    const transaction = await Transaction.find({ userId: req.user._id }).sort({
+    const transactions = await Transaction.find({ userId: req.user._id }).sort({
       date: -1,
     });
     res.json(transactions);
@@ -36,4 +36,55 @@ const createTransaction = async (req, res) => {
   }
 };
 
-module.exports = { getTransactions, createTransaction };
+const updateTransaction = async (req, res) => {
+  const { id } = req.params;
+  const { type, category, amount, date, note } = req.body;
+
+  try {
+    const transaction = await Transaction.findOne({
+      _id: id,
+      userId: req.user._id,
+    });
+    if (!transaction) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+    transaction.type = type || transaction.type;
+    transaction.category = category || transaction.category;
+    transaction.amount = amount || transaction.amount;
+    transaction.date = date || transaction.date;
+    transaction.note = note || transaction.note;
+
+    await transaction.save();
+    res.json(transaction);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const deleteTransaction = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const transaction = await Transaction.findOne({
+      _id: id,
+      userId: req.user._id,
+    });
+    if (!transaction) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    await transaction.remove();
+    res.json({ message: "Transaction removed" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = {
+  getTransactions,
+  createTransaction,
+  updateTransaction,
+  deleteTransaction,
+};
--- scr/routes/transactionRoutes.js
+++ scr/routes/transactionRoutes.js
@@ -4,11 +4,15 @@ const protect = require("../middleware/auth");
 const {
   getTransactions,
   createTransaction,
+  updateTransaction,
+  deleteTransaction,
 } = require("../controllers/transactionController");
 
 router.use(protect);
 
 router.get("/", getTransactions);
 router.post("/", createTransaction);
+router.put("/", updateTransaction);
+router.delete("/", deleteTransaction);
 
 module.exports = router;
